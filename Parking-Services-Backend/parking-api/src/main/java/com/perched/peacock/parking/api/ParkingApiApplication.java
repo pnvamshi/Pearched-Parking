fafce5a2c1709c038ab2ecaea2443f223e32f798
Vamshi P.N <pnvamshi9@gmail.com>
2019-10-13 19:31:24 +0530

    Added New Dependencies

--- Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/ParkingApiApplication.java
+++ Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/ParkingApiApplication.java
@@ -1,12 +1,25 @@
 package com.perched.peacock.parking.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author pnvamshi
+ *
+ */
 
 @SpringBootApplication
 public class ParkingApiApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ParkingApiApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApiApplication.class, args);
+		LOGGER.info("**********************************************************************");
+		LOGGER.info("************PARKING API SERVICE STARTED SUCCESSFULLY *****************"); 
+		LOGGER.info("**********************************************************************");
 	}
 }
