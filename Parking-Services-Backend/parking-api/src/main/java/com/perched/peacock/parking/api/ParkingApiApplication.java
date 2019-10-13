b43a7a49ae5497941ce7796eebe0c28c73315d0f
Vamshi P.N <pnvamshi9@gmail.com>
2019-10-13 17:41:37 +0530

    Application Properties

---
+++ Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/ParkingApiApplication.java
@@ -0,0 +1,12 @@
+package com.perched.peacock.parking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApiApplication.class, args);
+	}
+}
