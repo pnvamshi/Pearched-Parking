2b770b6a36d85a2b1e58c3bf916a22864d7b232b
Vamshi P.N <pnvamshi9@gmail.com>
2019-10-19 16:27:12 +0530

    Update Mongo Connection

--- Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/mongo/dao/ParkingSlotsDAO.java
+++ Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/mongo/dao/ParkingSlotsDAO.java
@@ -1,5 +1,9 @@
 package com.perched.peacock.parking.api.mongo.dao;
 
+import java.util.List;
+
+import com.perched.peacock.parking.api.mongo.model.ParkingSlots;
+
 /**
  * 
  * @author pnvamshi
@@ -7,5 +11,18 @@ package com.perched.peacock.parking.api.mongo.dao;
  */
 
 public interface ParkingSlotsDAO {
+
+	/**
+	 * 
+	 * @param parkingRegion
+	 * @return
+	 */
+	ParkingSlots getParkingSlotsInfo(String parkingRegion);
+
+	/**
+	 * 
+	 * @return
+	 */
+	List<String> getParkingRegions();
 
 }
--- Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/mongo/dao/impl/ParkingSlotsDAOImpl.java
+++ Parking-Services-Backend/parking-api/src/main/java/com/perched/peacock/parking/api/mongo/dao/impl/ParkingSlotsDAOImpl.java
@@ -1,8 +1,15 @@
 package com.perched.peacock.parking.api.mongo.dao.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.perched.peacock.parking.api.mongo.dao.ParkingSlotsDAO;
+import com.perched.peacock.parking.api.mongo.model.ParkingSlots;
 
 /**
  * 
@@ -13,5 +20,18 @@ import com.perched.peacock.parking.api.mongo.dao.ParkingSlotsDAO;
 @Repository
 public class ParkingSlotsDAOImpl implements ParkingSlotsDAO {
 
-	 
+	@Autowired
+	MongoTemplate mongoTemplate;
+	
+	@Override
+	public ParkingSlots getParkingSlotsInfo(String parkingRegion) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("parkingRegion").is(parkingRegion));
+		return mongoTemplate.findOne(query, ParkingSlots.class);
+	}
+	
+	@Override
+	public List<String> getParkingRegions() {
+		return mongoTemplate.findDistinct("parkingRegion", ParkingSlots.class, String.class);
+	}
 }
